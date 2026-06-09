1a6d01dced56604f4426d5290380c3d64a67ee90
Hari Narayana Rath <119601501+Hari-Narayana-Rath@users.noreply.github.com>
2026-06-09 12:44:18 +0530

    Create check.py

---
+++ check.py
@@ -0,0 +1,248 @@
+# Generates 500 lines of harmless code
+
+value_1 = 1
+value_2 = 2
+value_3 = 3
+value_4 = 4
+value_5 = 5
+value_6 = 6
+value_7 = 7
+value_8 = 8
+value_9 = 9
+value_10 = 10
+
+def func_1():
+    return 1
+
+def func_2():
+    return 2
+
+def func_3():
+    return 3
+
+def func_4():
+    return 4
+
+def func_5():
+    return 5
+
+def func_6():
+    return 6
+
+def func_7():
+    return 7
+
+def func_8():
+    return 8
+
+def func_9():
+    return 9
+
+def func_10():
+    return 10
+
+class Dummy1:
+    def __init__(self):
+        self.value = 1
+
+    def get(self):
+        return self.value
+
+class Dummy2:
+    def __init__(self):
+        self.value = 2
+
+    def get(self):
+        return self.value
+
+class Dummy3:
+    def __init__(self):
+        self.value = 3
+
+    def get(self):
+        return self.value
+
+class Dummy4:
+    def __init__(self):
+        self.value = 4
+
+    def get(self):
+        return self.value
+
+class Dummy5:
+    def __init__(self):
+        self.value = 5
+
+    def get(self):
+        return self.value
+
+numbers = []
+
+for i in range(100):
+    numbers.append(i)
+
+total = 0
+
+for n in numbers:
+    total += n
+
+result_1 = total
+result_2 = total + 1
+result_3 = total + 2
+result_4 = total + 3
+result_5 = total + 4
+result_6 = total + 5
+result_7 = total + 6
+result_8 = total + 7
+result_9 = total + 8
+result_10 = total + 9
+
+data_1 = {"a": 1}
+data_2 = {"a": 2}
+data_3 = {"a": 3}
+data_4 = {"a": 4}
+data_5 = {"a": 5}
+
+text_1 = "line"
+text_2 = "line"
+text_3 = "line"
+text_4 = "line"
+text_5 = "line"
+
+buffer = []
+
+for i in range(50):
+    buffer.append(i * 2)
+
+for i in range(50):
+    buffer.append(i * 3)
+
+for i in range(50):
+    buffer.append(i * 4)
+
+for i in range(50):
+    buffer.append(i * 5)
+
+for i in range(50):
+    buffer.append(i * 6)
+
+def noop_1():
+    pass
+
+def noop_2():
+    pass
+
+def noop_3():
+    pass
+
+def noop_4():
+    pass
+
+def noop_5():
+    pass
+
+# Repeat similar harmless assignments to reach 500 lines
+
+line_001 = 1
+line_002 = 2
+line_003 = 3
+line_004 = 4
+line_005 = 5
+line_006 = 6
+line_007 = 7
+line_008 = 8
+line_009 = 9
+line_010 = 10
+line_011 = 11
+line_012 = 12
+line_013 = 13
+line_014 = 14
+line_015 = 15
+line_016 = 16
+line_017 = 17
+line_018 = 18
+line_019 = 19
+line_020 = 20
+line_021 = 21
+line_022 = 22
+line_023 = 23
+line_024 = 24
+line_025 = 25
+line_026 = 26
+line_027 = 27
+line_028 = 28
+line_029 = 29
+line_030 = 30
+line_031 = 31
+line_032 = 32
+line_033 = 33
+line_034 = 34
+line_035 = 35
+line_036 = 36
+line_037 = 37
+line_038 = 38
+line_039 = 39
+line_040 = 40
+line_041 = 41
+line_042 = 42
+line_043 = 43
+line_044 = 44
+line_045 = 45
+line_046 = 46
+line_047 = 47
+line_048 = 48
+line_049 = 49
+line_050 = 50
+
+# ... continue the same pattern ...
+
+line_451 = 451
+line_452 = 452
+line_453 = 453
+line_454 = 454
+line_455 = 455
+line_456 = 456
+line_457 = 457
+line_458 = 458
+line_459 = 459
+line_460 = 460
+line_461 = 461
+line_462 = 462
+line_463 = 463
+line_464 = 464
+line_465 = 465
+line_466 = 466
+line_467 = 467
+line_468 = 468
+line_469 = 469
+line_470 = 470
+line_471 = 471
+line_472 = 472
+line_473 = 473
+line_474 = 474
+line_475 = 475
+line_476 = 476
+line_477 = 477
+line_478 = 478
+line_479 = 479
+line_480 = 480
+line_481 = 481
+line_482 = 482
+line_483 = 483
+line_484 = 484
+line_485 = 485
+line_486 = 486
+line_487 = 487
+line_488 = 488
+line_489 = 489
+line_490 = 490
+line_491 = 491
+line_492 = 492
+line_493 = 493
+line_494 = 494
+line_495 = 495
+line_496 = 496
+line_497 = 497
+line_498 = 498
+line_499 = 499
+line_500 = 500
